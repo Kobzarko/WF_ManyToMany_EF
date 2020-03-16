09d6c11ae9bb766f45a524f70ce5c41e880559a5
Yevhen <evgewi4@gmail.com>
2020-03-16 22:54:19 +0200

    Добавьте файлы проекта.

---
+++ WF_ManyToMany_EF/Models/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace WF_ManyToMany_EF
+{
+  public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public virtual ICollection<Team> Teams { get; set; }
+
+        public Player()
+        {
+            Teams = new List<Team>();
+        }
+    }
+}
---
+++ WF_ManyToMany_EF/Models/SoccerContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+namespace WF_ManyToMany_EF.Models
+{
+    class SoccerContext:DbContext
+    {
+        public SoccerContext():base("SoccerDb")
+        {
+
+        }
+
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Player> Players { get; set; }
+    }
+}
